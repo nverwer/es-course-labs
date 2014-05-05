99a9edaf19b4a6f141266eed759000785f290dc5
Nico Verwer <nverwer@rakensi.com>
2014-05-05 17:40:55 +0200

    Indexing exercise.

---
+++ 02-index-more/index-data.js
@@ -0,0 +1,36 @@
+/*
+  Install dependencies:
+  * npm install request
+
+  Generate data and put it in data.json.
+  Change the name of the index and type before you run this exercise.
+*/
+
+var fs = require('fs');
+var request = require('request');
+
+var url = 'http://localhost:9200/insert-your-index-name/insert-your-type-name';
+
+var callback = function(error, response, result){
+  if (error) {
+    console.log("ERROR: "+error);
+  } else {
+    console.log("Response code: "+response.statusCode);
+    console.log(JSON.stringify(JSON.parse(result), null, 2));
+  }
+};
+
+
+// Read the data.
+fs.readFile('data.json', 'utf8', function(err, data){
+  // Handle error or index the data
+  if (err) throw err;
+  var documents = JSON.parse(data);
+  for (var i in documents) {
+    request.post({
+        url: url,
+        body: JSON.stringify(documents[i], null, 0)
+      }, callback);
+  }
+});
+
